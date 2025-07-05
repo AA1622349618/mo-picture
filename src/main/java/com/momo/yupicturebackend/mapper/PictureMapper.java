9cf93d49048fcd723c156154ff34dbffca8e7fcb
微草 <1622349618@qq.com>
2025-07-05 14:42:40 +0800

    完成4 - 图片模块

---
+++ src/main/java/com/momo/yupicturebackend/mapper/PictureMapper.java
@@ -0,0 +1,15 @@
+package com.momo.yupicturebackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.momo.yupicturebackend.model.entity.Picture;
+
+/**
+* @description 针对表【picture(图片)】的数据库操作Mapper
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
