7408863462f54c0f40dc1f549ed124fa92b78c1b
微草 <1622349618@qq.com>
2025-07-05 10:59:35 +0800

    完成4 - 图片模块

---
+++ src/main/java/com/momo/yupicturebackend/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.momo.yupicturebackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.momo.yupicturebackend.model.entity.User;
+
+/**
+* @description 针对表【user(用户)】的数据库操作Mapper
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
